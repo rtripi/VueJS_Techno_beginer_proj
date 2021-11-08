fdf40aebe3edd867f054f89e06ff8f7cd7afb4e5
Renato Tripi <renatosft@gmail.com>
2021-11-08 15:15:41 -0300

    criado modal com detalhes de produto

--- app.js
+++ app.js
@@ -2,12 +2,38 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: {},
+    produto: false,
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
   },
   methods: {
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
+    },
     fetchProdutos() {
       fetch("./api/produtos.json")
         .then((r) => r.json())
         .then((r) => (this.produtos = r));
+    },
+
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => (this.produto = r));
     },
   },
 
